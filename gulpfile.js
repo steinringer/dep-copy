e9c106107b316672c7437e93a7f7ddaa2f8e5eef
Jozef Lacny <jozef.lacny@steinringer.sk>
2015-08-12 14:12:08 +0200

    v1.0.4
    added example gulpfile
    fixed through2 dependency which was causing malfuncion

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+﻿var gulp = require('gulp'),
+	print = require('gulp-print'),
+	Installer = require('./dep-copy.js');
+
+var deployPath = './tests/testDeployFolder';
+var packageName = "EXAMPLE";
+
+var basePath = 'tests/testInputs';
+var filesToBeInstalled = [
+    './tests/testInputs/App/**/*.*',
+    './tests/testInputs/Configuration/**/*.json',
+    './tests/testInputs/bin/**/somedll.dll',
+    './tests/testInputs/bin/**/*' + packageName + '*.dll'
+];
+
+gulp.task('install', function () {
+    var installer = new Installer(basePath, packageName, deployPath);
+
+	gulp.src(filesToBeInstalled, { base: basePath })
+		.pipe(print())
+		.pipe(installer.copyFiles())
+		.pipe(installer.addToIndexFile());
+});
